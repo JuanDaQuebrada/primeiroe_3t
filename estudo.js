82a6d9a44de6d2f708ba915104b3764a2174db63
JuanDaQuebrada <110102077+JuanDaQuebrada@users.noreply.github.com>
2022-10-26 09:09:01 -0300

    Update estudo.js

--- estudo.js
+++ estudo.js
@@ -1,31 +1,95 @@
-<!DOCTYPE html>
-<html lang="pt-br" dir="ltr">
-  <head>
-    <meta charset="utf-8">
-    <meta http-equv="X-UA-Compatible" content="IE=edge"> 
-    <meta name="viewport" content="width=device-widh , initial-scale=1.0">
-    <title>Primeiro E</title>
-    <script src="estudo.js"></script>
-  </head>
+let tabuada = 2;
 
-  <body>
-    Valor: <input id="valor" type="text"><br>
-    Juros(Mensal): <input id="juros" type="text"><br>
-    Nr. Meses: <input id="meses" type="text"><br>
-    <button onclick="calcula()">Enviar</button>
+function escreva() {
+    document.write("Tabuada do " + tabuada + "<br>");
+    document.write(tabuada + " x 1 = " + (tabuada * 1) + "<br>");
+    document.write(tabuada + " x 2 = " + (tabuada * 2) + "<br>");
+    document.write(tabuada + " x 3 = " + (tabuada * 3) + "<br>");
+    document.write(tabuada + " x 4 = " + (tabuada * 4) + "<br>");
+    document.write(tabuada + " x 5 = " + (tabuada * 5) + "<br>");
+    document.write(tabuada + " x 6 = " + (tabuada * 6) + "<br>");
+    document.write(tabuada + " x 7 = " + (tabuada * 7) + "<br>");
+    document.write(tabuada + " x 8 = " + (tabuada * 8) + "<br>");
+    document.write(tabuada + " x 9 = " + (tabuada * 9) + "<br>");
+    document.write(tabuada + " x 10 = " + (tabuada * 10) + "<br>");
 
-    <br><br><br>
+}
 
-    Nota 1: <input id="t1" type="text"><br>
-    Nota 2: <input id="t2" type="text"><br>
-    Nota 3: <input id="t3" type="text"><br>
+function minhaTabuada() {
+    for (let i = 1; i <= 10; i++) {
+        document.write("Tabuada do " + i + "<br>");
+        for (let j = 1; j <= 10; j++) {
+            document.write(i + " x " + j + " = " + (j * i) + "<br>");
+        }
+        document.write("<br>");
+    }
+}
 
-    <button onclick="somarNotas()">Somar Notas</button>
-    <span id="resultadosoma">0</span><br>
-    <button onclick="mediaNotas()">Média Notas</button>
-    <span id="resultadomedia">0</span><br>
-    <button onclick="fecharNotas()">Notas para Fechar</button>
-    <span id="resultadofechar">0</span><br>
-  
-  </body>
-<html>
+function quadrado() {
+    for (let i = 1; i <= 10; i++) {
+        document.write("O quadrado de " + i + " é " + (i * i) + "<br>");
+    }
+}
+
+function moeda(atual) {
+    return atual.toLocaleString('pt-br', { style: 'currency', currency: 'BRL' });
+}
+
+function calcula() {
+    let val = document.getElementById("valor").value;
+    let j = document.getElementById("juros").value;
+    let t = document.getElementById("meses").value;
+    if (!Number(val)) {
+        alert("O valor deve ser um NÚMERO.");
+        document.getElementById("valor").value = "";
+        document.getElementById("valor").focus();
+        return
+    }
+    if (!Number(j)) {
+        alert("O valor dos juros deve ser um NÚMERO.");
+        document.getElementById("juros").value = "";
+        document.getElementById("juros").focus();
+        return
+    }
+    if (!Number(t)) {
+        alert("O valor do tempo deve ser um NÚMERO.");
+        document.getElementById("meses").value = "";
+        document.getElementById("meses").focus();
+        return
+    }
+
+    let res = val;
+    for (let m = 1; m <= t; m++) {
+        res = val * (1 + (j / 100));
+        val = res;
+       // document.write("Mês " + m + " = " + moeda(res) + "<br>")
+    }
+    document.getElementById("total").innerHTML= "Total: " + moeda(res);
+    //document.write("Resultado: " + moeda(res));
+}
+
+function somarNotas() {
+    let n1 = document.getElementById("t1").value;
+    let n2 = document.getElementById("t2").value;
+    let n3 = document.getElementById("t3").value;
+    let r = Number(n1) + Number(n2) + Number(n3);
+    document.getElementById("resultadosoma").innerHTML = "Soma: " + r;
+}
+
+function mediaNotas() {
+    let n1 = document.getElementById("t1").value;
+    let n2 = document.getElementById("t2").value;
+    let n3 = document.getElementById("t3").value;
+    let r = Number(n1) + Number(n2) + Number(n3);
+    r = r / Number(3)
+    document.getElementById("resultadomedia").innerHTML = "Média: " + r;
+}
+
+function fecharNotas() {
+    let n1 = document.getElementById("t1").value;
+    let n2 = document.getElementById("t2").value;
+    let n3 = document.getElementById("t3").value;
+    let r = Number(n1) + Number(n2) + Number(n3);
+    r = 180 - r;
+    document.getElementById("resultadofechar").innerHTML = "Falta: " + r;
+}
